a866aaf91ce7bb857067b19783001bb1930cbc1c
Thibaut Durand <durand.tibo@gmail.com>
2023-01-14 20:42:20 -0800

    Add CI workflows

--- src/gravitorch/utils/info.py
+++ src/gravitorch/utils/info.py
@@ -12,15 +12,15 @@ from gravitorch.utils.path import get_original_cwd
 
 logger = logging.getLogger(__name__)
 
-# ASCII logo generated from http://patorjk.com/software/taag/#p=testall&f=AMC%203%20Line&t=gravitorch
-MLTORCH_LOGO = rf"""
+# ASCII logo generated from https://ascii.co.uk/art/cats
+GRAVITORCH_LOGO = rf"""
 
-___  ___ _    _____              _
-|  \/  || |  |_   _|            | |
-| .  . || |    | | ___  _ __ ___| |__
-| |\/| || |    | |/ _ \| '__/ __| '_ \
-| |  | || |____| | (_) | | | (__| | | |
-\_|  |_/\_____/\_/\___/|_|  \___|_| |_|
+        /\_/\  /\          /\_/\  /\          /\_/\  /\          /\_/\  /\
+       / o o \ \ \        / o o \ \ \        / o o \ \ \        / o o \ \ \
+      /   Y   \/ /       /   Y   \/ /       /   Y   \/ /       /   Y   \/ /
+     /         \/       /         \/       /         \/       /         \/
+     \ | | | | /        \ | | | | /        \ | | | | /        \ | | | | /
+      `|_|-|_|'          `|_|-|_|'          `|_|-|_|'          `|_|-|_|'
 
 version: {gravitorch.__version__}"""
 
@@ -32,7 +32,7 @@ def log_run_info(config: DictConfig) -> None:
         config (``omegaconf.DictConfig``): Specifies the config of
             the run.
     """
-    logger.info(MLTORCH_LOGO)
+    logger.info(GRAVITORCH_LOGO)
     logger.info("Original working directory: %s", get_original_cwd())
     logger.info("Current working directory: %s", Path.cwd())
     logger.info("Config:\n%s", OmegaConf.to_yaml(config))
