dabf811240e0add898ceeec1ac3f0a984d1820c6
Thibaut Durand <durand.tibo@gmail.com>
2023-01-17 19:54:19 -0800

    Rename `Looper`

--- tests/unit/data/datapipes/iter/test_length.py
+++ tests/unit/data/datapipes/iter/test_length.py
@@ -1,41 +1,37 @@
 from pytest import mark, raises
 
-from gravitorch.data.datapipes.iter import FixedLengthIterDataPipe, SourceIterDataPipe
+from gravitorch.data.datapipes.iter import Looper, SourceIterDataPipe
 
-#############################################
-#     Tests for FixedLengthIterDataPipe     #
-#############################################
-
-
-def test_fixed_length_iter_datapipe_str():
-    assert str(FixedLengthIterDataPipe(SourceIterDataPipe([]), length=42)).startswith(
-        "FixedLengthIterDataPipe("
-    )
+############################
+#     Tests for Looper     #
+############################
 
 
-def test_fixed_length_iter_datapipe_iter_exact_length():
-    datapipe = FixedLengthIterDataPipe(SourceIterDataPipe([1, 2, 3]), length=3)
+def test_looper_str():
+    assert str(Looper(SourceIterDataPipe([]), length=42)).startswith("LooperIterDataPipe(")
+
+
+def test_looper_iter_exact_length():
+    datapipe = Looper(SourceIterDataPipe([1, 2, 3]), length=3)
     assert tuple(datapipe) == (1, 2, 3)
 
 
-def test_fixed_length_iter_datapipe_iter_too_long():
-    datapipe = FixedLengthIterDataPipe(SourceIterDataPipe([1, 2, 3, 4, 5]), length=3)
+def test_looper_iter_too_long():
+    datapipe = Looper(SourceIterDataPipe([1, 2, 3, 4, 5]), length=3)
     assert tuple(datapipe) == (1, 2, 3)
 
 
-def test_fixed_length_iter_datapipe_iter_too_short():
-    datapipe = FixedLengthIterDataPipe(SourceIterDataPipe([1, 2, 3]), length=5)
+def test_looper_iter_too_short():
+    datapipe = Looper(SourceIterDataPipe([1, 2, 3]), length=5)
     assert tuple(datapipe) == (1, 2, 3, 1, 2)
 
 
 @mark.parametrize("length", (1, 2, 3))
-def test_fixed_length_iter_datapipe_len(length: int):
-    assert (
-        len(FixedLengthIterDataPipe(SourceIterDataPipe([1, 2, 3, 4, 5]), length=length)) == length
-    )
+def test_looper_len(length: int):
+    assert len(Looper(SourceIterDataPipe([1, 2, 3, 4, 5]), length=length)) == length
 
 
 @mark.parametrize("length", (-1, 0))
-def test_fixed_length_iter_datapipe_incorrect_length(length: int):
+def test_looper_incorrect_length(length: int):
     with raises(ValueError):
-        FixedLengthIterDataPipe(SourceIterDataPipe([1, 2, 3, 4, 5]), length=length)
+        Looper(SourceIterDataPipe([1, 2, 3, 4, 5]), length=length)
