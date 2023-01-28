4857e4f3314c9cc6030743df9ddf4dbbdec9b99c
Thibaut Durand <durand.tibo@gmail.com>
2023-01-28 11:18:36 -0800

    Improve `PyTorchCudaBackend` (#29)

--- src/gravitorch/experimental/sysconfig/__init__.py
+++ src/gravitorch/experimental/sysconfig/__init__.py
@@ -1,7 +1,4 @@
-__all__ = ["BaseSysConfig", "PyTorchCudaBackend", "pytorch_cuda_backend"]
+__all__ = ["BaseSysConfig", "PyTorchCudaBackend"]
 
 from gravitorch.experimental.sysconfig.base import BaseSysConfig
-from gravitorch.experimental.sysconfig.pytorch import (
-    PyTorchCudaBackend,
-    pytorch_cuda_backend,
-)
+from gravitorch.experimental.sysconfig.pytorch import PyTorchCudaBackend
--- tests/unit/experimental/sysconfig/test_pytorch.py
+++ tests/unit/experimental/sysconfig/test_pytorch.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from pytest import LogCaptureFixture, mark
 from torch.backends import cuda
 
-from gravitorch.experimental.sysconfig import PyTorchCudaBackend, pytorch_cuda_backend
+from gravitorch.experimental.sysconfig import PyTorchCudaBackend
 
 ########################################
 #     Tests for PyTorchCudaBackend     #
@@ -16,20 +16,18 @@ def test_pytorch_cuda_backend_str():
 
 
 @mark.parametrize("allow_tf32", (True, False))
-def test_pytorch_cuda_backend_configure_allow_tf32(allow_tf32: bool):
-    with pytorch_cuda_backend():
-        PyTorchCudaBackend(allow_tf32=allow_tf32).configure()
+def test_pytorch_cuda_backend_allow_tf32(allow_tf32: bool):
+    with PyTorchCudaBackend(allow_tf32=allow_tf32):
         assert cuda.matmul.allow_tf32 == allow_tf32
 
 
 @mark.parametrize("allow_fp16_reduced_precision_reduction", (True, False))
-def test_pytorch_cuda_backend_configure_allow_fp16_reduced_precision_reduction(
+def test_pytorch_cuda_backend_allow_fp16_reduced_precision_reduction(
     allow_fp16_reduced_precision_reduction: bool,
 ):
-    with pytorch_cuda_backend():
-        PyTorchCudaBackend(
-            allow_fp16_reduced_precision_reduction=allow_fp16_reduced_precision_reduction
-        ).configure()
+    with PyTorchCudaBackend(
+        allow_fp16_reduced_precision_reduction=allow_fp16_reduced_precision_reduction
+    ):
         assert (
             cuda.matmul.allow_fp16_reduced_precision_reduction
             == allow_fp16_reduced_precision_reduction
@@ -37,28 +35,45 @@ def test_pytorch_cuda_backend_configure_allow_fp16_reduced_precision_reduction(
 
 
 @mark.parametrize("flash_sdp_enabled", (True, False))
-def test_pytorch_cuda_backend_configure_flash_sdp_enabled(flash_sdp_enabled: bool):
-    with pytorch_cuda_backend():
-        PyTorchCudaBackend(flash_sdp_enabled=flash_sdp_enabled).configure()
+def test_pytorch_cuda_backend_flash_sdp_enabled(flash_sdp_enabled: bool):
+    with PyTorchCudaBackend(flash_sdp_enabled=flash_sdp_enabled):
         assert cuda.flash_sdp_enabled() == flash_sdp_enabled
 
 
 @mark.parametrize("math_sdp_enabled", (True, False))
-def test_pytorch_cuda_backend_configure_math_sdp_enabled(math_sdp_enabled: bool):
-    with pytorch_cuda_backend():
-        PyTorchCudaBackend(math_sdp_enabled=math_sdp_enabled).configure()
+def test_pytorch_cuda_backend_math_sdp_enabled(math_sdp_enabled: bool):
+    with PyTorchCudaBackend(math_sdp_enabled=math_sdp_enabled):
         assert cuda.math_sdp_enabled() == math_sdp_enabled
 
 
 @mark.parametrize("preferred_linalg_backend", ("cusolver", "magma", "default"))
-def test_pytorch_cuda_backend_configure_preferred_linalg_backend(preferred_linalg_backend: str):
-    sysconfig = PyTorchCudaBackend(preferred_linalg_backend=preferred_linalg_backend)
+def test_pytorch_cuda_backend_preferred_linalg_backend(preferred_linalg_backend: str):
     with patch("torch.backends.cuda.preferred_linalg_library") as mock:
-        sysconfig.configure()
-        mock.assert_called_once_with(preferred_linalg_backend)
+        with PyTorchCudaBackend(preferred_linalg_backend=preferred_linalg_backend):
+            mock.assert_called()
+
+
+@mark.parametrize("preferred_linalg_backend", ("cusolver", "magma", "default"))
+def test_pytorch_cuda_backend_configure_preferred_linalg_backend(preferred_linalg_backend: str):
+    with patch("torch.backends.cuda.preferred_linalg_library"):
+        with PyTorchCudaBackend(preferred_linalg_backend=preferred_linalg_backend) as sysconfig:
+            with patch("torch.backends.cuda.preferred_linalg_library") as mock:
+                sysconfig.configure()
+                mock.assert_called_once_with(preferred_linalg_backend)
 
 
 def test_pytorch_cuda_backend_show(caplog: LogCaptureFixture):
+    sysconfig = PyTorchCudaBackend()
     with caplog.at_level(logging.INFO):
-        PyTorchCudaBackend().show()
-        assert len(caplog.messages[0]) > 0  # The message should not be empty
+        sysconfig.show()
+        assert caplog.messages
+
+
+def test_pytorch_cuda_backend_show_state_true(caplog: LogCaptureFixture):
+    with PyTorchCudaBackend(show_state=True):
+        assert caplog.messages
+
+
+def test_pytorch_cuda_backend_show_state_false(caplog: LogCaptureFixture):
+    with PyTorchCudaBackend():
+        assert not caplog.messages
