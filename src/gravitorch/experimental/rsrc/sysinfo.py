521c303bd7f616111588e4053b450c61bd4e5e18
Thibaut Durand <durand.tibo@gmail.com>
2023-01-31 20:21:00 -0800

    Add `LogCudaMemory`

--- src/gravitorch/experimental/rsrc/sysinfo.py
+++ src/gravitorch/experimental/rsrc/sysinfo.py
@@ -1,13 +1,33 @@
-__all__ = ["LogSysInfo"]
+__all__ = ["LogCudaMemory", "LogSysInfo"]
 
 import logging
 from types import TracebackType
 from typing import Optional
 
 from gravitorch.experimental.rsrc.base import BaseResource
+from gravitorch.utils.cuda_memory import log_max_cuda_memory_allocated
 from gravitorch.utils.sysinfo import log_system_info
 
 logger = logging.getLogger(__name__)
+
+
+class LogCudaMemory(BaseResource):
+    r"""Implements a context manager to log the CUDA memory."""
+
+    def __enter__(self) -> "LogCudaMemory":
+        log_max_cuda_memory_allocated()
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        log_max_cuda_memory_allocated()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}()"
 
 
 class LogSysInfo(BaseResource):
