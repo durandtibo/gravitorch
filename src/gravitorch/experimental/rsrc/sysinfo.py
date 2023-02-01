de18ca072a35400febcbae3a03fd5ce490e63c4f
Thibaut Durand <durand.tibo@gmail.com>
2023-01-31 20:07:14 -0800

    Add `log_system_info`

--- src/gravitorch/experimental/rsrc/sysinfo.py
+++ src/gravitorch/experimental/rsrc/sysinfo.py
@@ -5,11 +5,7 @@ from types import TracebackType
 from typing import Optional
 
 from gravitorch.experimental.rsrc.base import BaseResource
-from gravitorch.utils.sysinfo import (
-    cpu_human_summary,
-    swap_memory_human_summary,
-    virtual_memory_human_summary,
-)
+from gravitorch.utils.sysinfo import log_system_info
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +14,7 @@ class LogSysInfo(BaseResource):
     r"""Implements a context manager to log system information."""
 
     def __enter__(self) -> "LogSysInfo":
-        self._show()
+        log_system_info()
         return self
 
     def __exit__(
@@ -27,12 +23,7 @@ class LogSysInfo(BaseResource):
         exc_val: Optional[BaseException],
         exc_tb: Optional[TracebackType],
     ) -> None:
-        self._show()
+        log_system_info()
 
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}()"
-
-    def _show(self) -> None:
-        logger.info(cpu_human_summary())
-        logger.info(virtual_memory_human_summary())
-        logger.info(swap_memory_human_summary())
--- tests/unit/utils/test_sysinfo.py
+++ tests/unit/utils/test_sysinfo.py
@@ -1,5 +1,10 @@
+import logging
+
+from pytest import LogCaptureFixture
+
 from gravitorch.utils.sysinfo import (
     cpu_human_summary,
+    log_system_info,
     swap_memory_human_summary,
     virtual_memory_human_summary,
 )
@@ -11,6 +16,17 @@ from gravitorch.utils.sysinfo import (
 
 def test_cpu_human_summary():
     assert cpu_human_summary().startswith("CPU")
+
+
+#####################################
+#     Tests for log_system_info     #
+#####################################
+
+
+def test_log_system_info(caplog: LogCaptureFixture):
+    with caplog.at_level(logging.INFO):
+        log_system_info()
+    assert len(caplog.messages) == 3
 
 
 ###############################################
