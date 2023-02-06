111cb55dd1973bf4c3f5f187fa86102dc9e251f2
Thibaut Durand <durand.tibo@gmail.com>
2023-02-05 17:44:00 -0800

    Add new version of `VanillaTrainingLoop`

--- src/gravitorch/loops/training/__init__.py
+++ src/gravitorch/loops/training/__init__.py
@@ -1,3 +1,5 @@
-__all__ = ["BaseTrainingLoop"]
+__all__ = ["BaseTrainingLoop", "BaseBasicTrainingLoop", "VanillaTrainingLoop"]
 
 from gravitorch.loops.training.base import BaseTrainingLoop
+from gravitorch.loops.training.basic import BaseBasicTrainingLoop
+from gravitorch.loops.training.vanilla import VanillaTrainingLoop
