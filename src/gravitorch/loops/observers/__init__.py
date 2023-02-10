37874047d5c1539a3de62bb5194918c87b83b603
Thibaut Durand <durand.tibo@gmail.com>
2023-02-09 21:27:04 -0800

    Add sequential loop observer (#90)

--- src/gravitorch/loops/observers/__init__.py
+++ src/gravitorch/loops/observers/__init__.py
@@ -1,6 +1,13 @@
-__all__ = ["BaseLoopObserver", "NoOpLoopObserver", "PyTorchBatchSaver", "setup_loop_observer"]
+__all__ = [
+    "BaseLoopObserver",
+    "NoOpLoopObserver",
+    "PyTorchBatchSaver",
+    "Sequential",
+    "setup_loop_observer",
+]
 
 from gravitorch.loops.observers.base import BaseLoopObserver
 from gravitorch.loops.observers.batch_saving import PyTorchBatchSaver
 from gravitorch.loops.observers.factory import setup_loop_observer
 from gravitorch.loops.observers.noop import NoOpLoopObserver
+from gravitorch.loops.observers.sequential import Sequential
