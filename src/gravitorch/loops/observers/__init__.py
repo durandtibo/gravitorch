1676aaa92a6dfd7a080d62dea32793647da47bdf
Thibaut Durand <durand.tibo@gmail.com>
2023-02-11 12:56:09 -0800

    Rename sequential loop observer to avoid confusion (#91)

--- src/gravitorch/loops/observers/__init__.py
+++ src/gravitorch/loops/observers/__init__.py
@@ -2,7 +2,7 @@ __all__ = [
     "BaseLoopObserver",
     "NoOpLoopObserver",
     "PyTorchBatchSaver",
-    "Sequential",
+    "SequentialLoopObserver",
     "setup_loop_observer",
 ]
 
@@ -10,4 +10,4 @@ from gravitorch.loops.observers.base import BaseLoopObserver
 from gravitorch.loops.observers.batch_saving import PyTorchBatchSaver
 from gravitorch.loops.observers.factory import setup_loop_observer
 from gravitorch.loops.observers.noop import NoOpLoopObserver
-from gravitorch.loops.observers.sequential import Sequential
+from gravitorch.loops.observers.sequential import SequentialLoopObserver
