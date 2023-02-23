86a56f2ead615558740610828e87af0b056e7a0e
Thibaut Durand <durand.tibo@gmail.com>
2023-02-22 22:09:50 -0800

    Add some pytest decorators to `testing`

--- src/gravitorch/testing/__init__.py
+++ src/gravitorch/testing/__init__.py
@@ -1,11 +1,33 @@
 __all__ = [
-    "DummyDataset",
-    "DummyIterableDataset",
     "DummyClassificationModel",
     "DummyDataSource",
+    "DummyDataset",
+    "DummyIterableDataset",
+    "accelerate_available",
     "create_dummy_engine",
+    "cuda_available",
+    "distributed_available",
+    "gloo_available",
+    "nccl_available",
+    "pillow_available",
+    "psutil_available",
+    "tensorboard_available",
+    "torchvision_available",
+    "two_gpus_available",
 ]
 
+from gravitorch.testing._pytest import (
+    accelerate_available,
+    cuda_available,
+    distributed_available,
+    gloo_available,
+    two_gpus_available,
+    nccl_available,
+    pillow_available,
+    psutil_available,
+    tensorboard_available,
+    torchvision_available,
+)
 from gravitorch.testing.dummy import (
     DummyClassificationModel,
     DummyDataset,
