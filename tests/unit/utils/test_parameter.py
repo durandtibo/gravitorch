08da028aee822efd81cf6d93a14a9aeaa910f62b
Thibaut Durand <durand.tibo@gmail.com>
2023-02-22 18:13:46 -0800

    Add `is_uninitialized_parameter`

---
+++ tests/unit/utils/test_parameter.py
@@ -0,0 +1,33 @@
+import torch
+from pytest import mark
+from torch.nn import Linear, Parameter, UninitializedParameter
+
+from gravitorch.utils.parameter import is_parameter, is_uninitialized_parameter
+
+##################################
+#     Tests for is_parameter     #
+##################################
+
+
+@mark.parametrize("value", (Parameter(torch.ones(2, 3)), UninitializedParameter()))
+def test_is_parameter_true(value: Parameter):
+    assert is_parameter(value)
+
+
+@mark.parametrize("value", (torch.ones(2, 3), Linear(4, 6), "abc"))
+def test_is_parameter_false(value: Parameter):
+    assert not is_parameter(value)
+
+
+################################################
+#     Tests for is_uninitialized_parameter     #
+################################################
+
+
+def test_is_uninitialized_parameter_true():
+    assert is_uninitialized_parameter(UninitializedParameter())
+
+
+@mark.parametrize("value", (Parameter(torch.ones(2, 3)), torch.ones(2, 3), Linear(4, 6), "abc"))
+def test_is_uninitialized_parameter_false(value: Parameter):
+    assert not is_uninitialized_parameter(value)
