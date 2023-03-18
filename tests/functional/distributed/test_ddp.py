93b8026234ecfd81b342edfa6e394d6a519728cf
Thibaut Durand <durand.tibo@gmail.com>
2023-03-18 14:36:43 -0700

    Move distributed tests to functional (#165)

--- tests/functional/distributed/test_ddp.py
+++ tests/functional/distributed/test_ddp.py
@@ -1,4 +1,3 @@
-import platform
 from collections.abc import Callable
 
 import torch
@@ -15,9 +14,6 @@ from gravitorch.testing import (
     nccl_available,
     two_gpus_available,
 )
-
-xfail_linux = mark.skipif(platform.system() == "Linux", reason="unstable tests for Linux")
-
 
 ###########################################
 #     Tests for broadcast_object_list     #
@@ -58,14 +54,12 @@ def check_broadcast_object_list(local_rank: int) -> None:
     assert objects_are_equal(object_list, [2, torch.tensor([0, 1], device=device)])
 
 
-@xfail_linux
 @distributed_available
 @gloo_available
 def test_broadcast_object_list_gloo(parallel_gloo_2: Parallel) -> None:
     parallel_gloo_2.run(check_sync_reduce_inplace)
 
 
-@xfail_linux
 @two_gpus_available
 @distributed_available
 @nccl_available
@@ -289,7 +283,6 @@ def check_sync_reduce_inplace(local_rank: int) -> None:
         ddp.sync_reduce_(2, op=ddp.SUM)  # Does not support integer
 
 
-@xfail_linux
 @mark.parametrize(
     "func",
     [
@@ -305,14 +298,12 @@ def test_sync_reduce_gloo(parallel_gloo_2: Parallel, func: Callable) -> None:
     parallel_gloo_2.run(func)
 
 
-@xfail_linux
 @distributed_available
 @gloo_available
 def test_sync_reduce_inplace_gloo(parallel_gloo_2: Parallel) -> None:
     parallel_gloo_2.run(check_sync_reduce_inplace)
 
 
-@xfail_linux
 @mark.parametrize(
     "func",
     [
@@ -329,7 +320,6 @@ def test_sync_reduce_nccl(parallel_nccl_2: Parallel, func: Callable) -> None:
     parallel_nccl_2.run(func)
 
 
-@xfail_linux
 @two_gpus_available
 @distributed_available
 @nccl_available
@@ -375,14 +365,12 @@ def check_all_gather_tensor_varshape(local_rank: int) -> None:
     )
 
 
-@xfail_linux
 @distributed_available
 @gloo_available
 def test_all_gather_tensor_varshape_gloo(parallel_gloo_2: Parallel) -> None:
     parallel_gloo_2.run(check_all_gather_tensor_varshape)
 
 
-@xfail_linux
 @two_gpus_available
 @distributed_available
 @nccl_available
