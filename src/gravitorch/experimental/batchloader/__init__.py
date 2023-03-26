ebb0fd86bbd8cd0c5ecdefe1f45f7ee7000b01f5
Thibaut Durand <durand.tibo@gmail.com>
2023-03-25 21:56:27 -0700

    Some improvements for `batchloader`

--- src/gravitorch/experimental/batchloader/__init__.py
+++ src/gravitorch/experimental/batchloader/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["BaseBatchLoader", "IterableBatchLoader"]
+__all__ = ["BaseBatchLoader", "DataLoader2BatchLoader", "IterableBatchLoader"]
 
 from gravitorch.experimental.batchloader.base import BaseBatchLoader
+from gravitorch.experimental.batchloader.dl2 import DataLoader2BatchLoader
 from gravitorch.experimental.batchloader.iterable import IterableBatchLoader
