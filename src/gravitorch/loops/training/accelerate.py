ddac1772479d2f0e9c2b75295372f1719e3bae2b
Thibaut Durand <durand.tibo@gmail.com>
2023-06-26 18:04:15 -0700

    Upgrade to `accelerate>=0.20.3` (#280)

--- src/gravitorch/loops/training/accelerate.py
+++ src/gravitorch/loops/training/accelerate.py
@@ -109,11 +109,9 @@ class AccelerateTrainingLoop(BaseBasicTrainingLoop):
         """
         logger.info("Preparing the model, optimizer, and data loader...")
         model, optimizer, data_loader = self._accelerator.prepare(
-            [
-                engine.model,
-                engine.optimizer,
-                engine.data_source.get_data_loader(loader_id=self._tag, engine=engine),
-            ],
+            engine.model,
+            engine.optimizer,
+            engine.data_source.get_data_loader(loader_id=self._tag, engine=engine),
         )
         prefix = f"({dist.get_rank()}/{dist.get_world_size()}) " if dist.is_distributed() else ""
         data_loader = tqdm(
