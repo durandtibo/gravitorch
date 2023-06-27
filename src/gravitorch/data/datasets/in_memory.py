d187c03ed02c0d27ce6b1862cfdfcd9d7b4fb917
Thibaut Durand <durand.tibo@gmail.com>
2023-06-26 19:23:19 -0700

    Remove `FileToInMemoryDataset` (#282)

--- src/gravitorch/data/datasets/in_memory.py
+++ src/gravitorch/data/datasets/in_memory.py
@@ -2,7 +2,7 @@ r"""This module implements datasets that stores all the examples in
 memory."""
 from __future__ import annotations
 
-__all__ = ["InMemoryDataset", "FileToInMemoryDataset"]
+__all__ = ["InMemoryDataset"]
 
 import logging
 from collections.abc import Sequence
@@ -140,70 +140,3 @@ class InMemoryDataset(Dataset[T]):
             >>> dataset = InMemoryDataset.from_pytorch_file("/path/to/file.pt")
         """
         return cls(torch.load(sanitize_path(path), **kwargs))
-
-
-class FileToInMemoryDataset(Dataset[T]):
-    r"""Implements a dataset that loads examples from a file and store
-    them in memory.
-
-    The data in the files should already be preprocessed and organized
-    by examples. The file should store a tuple (or list) of examples.
-    You can use this dataset only if all the examples can fit
-    in-memory.
-
-    This dataset supports the following file formats:
-
-        - PyTorch (file created by ``torch.save``): the extension of
-            the file has to be ``.pt``
-        - pickle: the extension of the file has to be ``.pkl``
-        - json: the extension of the file has to be ``.json``
-
-    The extension of the file is used to find the loader to use.
-
-    Args:
-    ----
-        path (``pathlib.Path`` or str): Specifies the path to the file
-            to load.
-    """
-
-    def __init__(self, path: Path | str) -> None:
-        log_box_dataset_class(self)
-        self._path = sanitize_path(path)
-        logger.info(f"Loading data from: {self._path}")
-        self._examples = _load_examples(self._path)
-
-    def __len__(self) -> int:
-        return len(self._examples)
-
-    def __getitem__(self, item: int) -> T:
-        return self._examples[item]
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(num_examples={len(self):,}, path={self._path})"
-
-
-def _load_examples(path: Path) -> tuple[T, ...]:
-    r"""Loads the examples from a file.
-
-    This function supports the following file formats:
-
-        - PyTorch (file created by ``torch.save``): the extension of
-            the file has to be ``.pt``
-        - pickle: the extension of the file has to be ``.pkl``
-        - json: the extension of the file has to be ``.json``
-
-    Args:
-    ----
-        path (``pathlib.Path`` or str): Specifies the path to the file
-            to load.
-    """
-    if path.suffix == ".pt":
-        return tuple(torch.load(path))
-    if path.suffix == ".pkl":
-        return tuple(load_pickle(path))
-    if path.suffix == ".json":
-        return tuple(load_json(path))
-    raise ValueError(
-        f"Incorrect file extension '{path.suffix}'. The supported file extensions "
-        "are '.pt', '.pkl' and '.json'"
-    )
--- tests/unit/data/datasets/test_in_memory.py
+++ tests/unit/data/datasets/test_in_memory.py
@@ -1,10 +1,9 @@
 from collections.abc import Sequence
 from pathlib import Path
 
-from pytest import mark, raises
+from pytest import mark
 
 from gravitorch.data.datasets import InMemoryDataset
-from gravitorch.data.datasets.in_memory import FileToInMemoryDataset, _load_examples
 from gravitorch.utils.io import save_json, save_pickle, save_pytorch
 
 #####################################
@@ -64,60 +63,3 @@ def test_in_memory_dataset_from_pytorch_file(tmp_path: Path) -> None:
     path = tmp_path.joinpath("data.pt")
     save_pytorch((1, 2), path)
     assert InMemoryDataset.from_pytorch_file(path).equal(InMemoryDataset((1, 2)))
-
-
-###########################################
-#     Tests for FileToInMemoryDataset     #
-###########################################
-
-
-def test_file_to_in_memory_dataset_str(tmp_path: Path) -> None:
-    path = tmp_path.joinpath("data.pt")
-    save_pytorch((1, 2), path)
-    assert str(FileToInMemoryDataset(path)).startswith("FileToInMemoryDataset")
-
-
-@mark.parametrize("examples,length", (((), 0), ((1,), 1), ((1, 2), 2)))
-def test_file_to_in_memory_dataset_len(tmp_path: Path, examples: Sequence, length: int) -> None:
-    path = tmp_path.joinpath("data.pt")
-    save_pytorch(examples, path)
-    assert len(FileToInMemoryDataset(path)) == length
-
-
-def test_file_to_in_memory_dataset_getitem(tmp_path: Path) -> None:
-    path = tmp_path.joinpath("data.pt")
-    save_pytorch((1, 2), path)
-    dataset = FileToInMemoryDataset(path)
-    assert dataset[0] == 1
-    assert dataset[1] == 2
-
-
-####################################
-#     Tests for _load_examples     #
-####################################
-
-
-@mark.parametrize("examples", ((1, 2, 3), [1, 2, 3]))
-def test_load_examples_json(tmp_path: Path, examples: Sequence) -> None:
-    path = tmp_path.joinpath("data.json")
-    save_json(examples, path)
-    assert _load_examples(path) == (1, 2, 3)
-
-
-@mark.parametrize("examples", ((1, 2, 3), [1, 2, 3]))
-def test_load_examples_pkl(tmp_path: Path, examples: Sequence) -> None:
-    path = tmp_path.joinpath("data.pkl")
-    save_pickle(examples, path)
-    assert _load_examples(path) == (1, 2, 3)
-
-
-@mark.parametrize("examples", ((1, 2, 3), [1, 2, 3]))
-def test_load_examples_pt(tmp_path: Path, examples: Sequence) -> None:
-    path = tmp_path.joinpath("data.pt")
-    save_pytorch(examples, path)
-    assert _load_examples(path) == (1, 2, 3)
-
-
-def test_load_examples_incorrect_extension(tmp_path: Path) -> None:
-    with raises(ValueError, match="Incorrect file extension '.something'"):
-        _load_examples(tmp_path.joinpath("data.something"))
