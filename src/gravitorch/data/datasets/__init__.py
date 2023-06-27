fa29e7e7ab2653808781093d121dbe8ed3f04fde
Thibaut Durand <durand.tibo@gmail.com>
2023-06-26 19:26:45 -0700

    Rename `ExampleDataset`

--- src/gravitorch/data/datasets/__init__.py
+++ src/gravitorch/data/datasets/__init__.py
@@ -12,8 +12,8 @@ __all__ = [
 ]
 
 from gravitorch.data.datasets.dummy import DummyMultiClassDataset
+from gravitorch.data.datasets.example import ExampleDataset
 from gravitorch.data.datasets.factory import setup_dataset
 from gravitorch.data.datasets.image_folder import ImageFolderDataset
-from gravitorch.data.datasets.in_memory import ExampleDataset
 from gravitorch.data.datasets.mnist import MNIST
 from gravitorch.data.datasets.utils import log_box_dataset_class
