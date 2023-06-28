05ebb369fdb57a339fe71ab2256c8e88560ecc77
Thibaut Durand <durand.tibo@gmail.com>
2023-06-28 07:17:32 -0700

    Remove `MNIST` dataset (#288)

--- src/gravitorch/data/datasets/__init__.py
+++ src/gravitorch/data/datasets/__init__.py
@@ -6,7 +6,6 @@ __all__ = [
     "DummyMultiClassDataset",
     "ImageFolderDataset",
     "ExampleDataset",
-    "MNIST",
     "create_datasets",
     "log_box_dataset_class",
     "setup_dataset",
@@ -16,5 +15,4 @@ from gravitorch.data.datasets.dummy import DummyMultiClassDataset
 from gravitorch.data.datasets.example import ExampleDataset
 from gravitorch.data.datasets.factory import create_datasets, setup_dataset
 from gravitorch.data.datasets.image_folder import ImageFolderDataset
-from gravitorch.data.datasets.mnist import MNIST
 from gravitorch.data.datasets.utils import log_box_dataset_class
