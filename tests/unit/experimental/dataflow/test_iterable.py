0e5313f4c445a87160e0a92fc3f0183d045a94f6
Thibaut Durand <durand.tibo@gmail.com>
2023-06-28 19:55:50 -0700

    Add `deepcopy` option to `IterableDataFlow` (#292)

--- tests/unit/experimental/dataflow/test_iterable.py
+++ tests/unit/experimental/dataflow/test_iterable.py
@@ -19,3 +19,29 @@ def test_iterable_dataflow_incorrect_type() -> None:
 def test_iterable_dataflow_iter() -> None:
     with IterableDataFlow([1, 2, 3, 4, 5]) as flow:
         assert list(flow) == [1, 2, 3, 4, 5]
+
+
+def test_iterable_dataflow_iter_deepcopy_true() -> None:
+    dataflow = IterableDataFlow([[1, 2, 3], [4, 5, 6], [7, 8], [9]], deepcopy=True)
+    with dataflow as flow:
+        for batch in flow:
+            batch.append(0)
+    with dataflow as flow:
+        assert list(flow) == [[1, 2, 3], [4, 5, 6], [7, 8], [9]]
+
+
+def test_iterable_dataflow_iter_deepcopy_false() -> None:
+    dataflow = IterableDataFlow([[1, 2, 3], [4, 5, 6], [7, 8], [9]])
+    with dataflow as flow:
+        for batch in flow:
+            batch.append(0)
+    with dataflow as flow:
+        assert list(flow) == [[1, 2, 3, 0], [4, 5, 6, 0], [7, 8, 0], [9, 0]]
+
+
+def test_iterable_dataflow_iter_impossible_deepcopy() -> None:
+    dataflow = IterableDataFlow((i for i in range(5)), deepcopy=True)
+    with dataflow as flow:
+        assert list(flow) == [0, 1, 2, 3, 4]
+    with dataflow as flow:
+        assert list(flow) == []
