9dac85a0afa3f76b4e8c4395d86a853dc3921654
Thibaut Durand <durand.tibo@gmail.com>
2023-06-28 19:46:03 -0700

    Add `deepcopy` option to `IterableDataFlow`

--- src/gravitorch/experimental/dataflow/iterable.py
+++ src/gravitorch/experimental/dataflow/iterable.py
@@ -2,13 +2,28 @@ from __future__ import annotations
 
 __all__ = ["IterableDataFlow"]
 
+import copy
+import logging
 from collections.abc import Iterable, Iterator
+from typing import TypeVar
 
 from gravitorch.experimental.dataflow.base import BaseDataFlow
 
+logger = logging.getLogger(__name__)
 
-class IterableDataFlow(BaseDataFlow):
+T = TypeVar("T")
+
+
+class IterableDataFlow(BaseDataFlow[T]):
     r"""Implements a simple dataflow for iterables.
+
+    Args:
+    ----
+        iterable (``Iterable``): Specifies the iterable.
+        deepcopy (bool, optional): If ``True``, the input iterable
+            object is deep-copied before to iterate over the data.
+            It allows a deterministic behavior when in-place
+            operations are performed on the data. Default: ``False``
 
     Example usage:
 
@@ -21,13 +36,24 @@ class IterableDataFlow(BaseDataFlow):
         ...
     """
 
-    def __init__(self, iterable: Iterable) -> None:
+    def __init__(self, iterable: Iterable[T], deepcopy: bool = False) -> None:
         if not isinstance(iterable, Iterable):
             raise TypeError(f"Incorrect type. Expecting iterable but received {type(iterable)}")
         self.iterable = iterable
+        self._deepcopy = bool(deepcopy)
 
-    def __iter__(self) -> Iterator:
-        return iter(self.iterable)
+    def __iter__(self) -> Iterator[T]:
+        iterable = self.iterable
+        if self._deepcopy:  # TODO: move to launch
+            logger.info("Copying the input iterable...")
+            try:
+                iterable = copy.deepcopy(iterable)
+            except TypeError:
+                logger.warning(
+                    "The input iterable can not be deepcopied, please be aware of in-place "
+                    "modification would affect source data."
+                )
+        yield from iterable
 
     def __str__(self) -> str:
         return f"{self.__class__.__qualname__}()"
