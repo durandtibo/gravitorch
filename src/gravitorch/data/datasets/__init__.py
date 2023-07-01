9d2a3f94abfc32161cb2591eff094c74af480a01
Thibaut Durand <durand.tibo@gmail.com>
2023-07-01 13:50:20 -0700

    Add `is_dataset_config`

--- src/gravitorch/data/datasets/__init__.py
+++ src/gravitorch/data/datasets/__init__.py
@@ -7,12 +7,17 @@ __all__ = [
     "ImageFolderDataset",
     "ExampleDataset",
     "create_datasets",
+    "is_dataset_config",
     "log_box_dataset_class",
     "setup_dataset",
 ]
 
 from gravitorch.data.datasets.dummy import DummyMultiClassDataset
 from gravitorch.data.datasets.example import ExampleDataset
-from gravitorch.data.datasets.factory import create_datasets, setup_dataset
+from gravitorch.data.datasets.factory import (
+    create_datasets,
+    is_dataset_config,
+    setup_dataset,
+)
 from gravitorch.data.datasets.image_folder import ImageFolderDataset
 from gravitorch.data.datasets.utils import log_box_dataset_class
