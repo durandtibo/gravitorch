3a4b5dc6ebd2dbddb3b14c7fad3950afb0a4d58a
Thibaut Durand <durand.tibo@gmail.com>
2023-07-02 21:50:34 -0700

    Add `clone_datapipe` (#314)

--- src/gravitorch/datapipes/__init__.py
+++ src/gravitorch/datapipes/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["setup_datapipe"]
+__all__ = ["clone_datapipe", "setup_datapipe"]
 
 from gravitorch.datapipes.factory import setup_datapipe
+from gravitorch.datapipes.utils import clone_datapipe
