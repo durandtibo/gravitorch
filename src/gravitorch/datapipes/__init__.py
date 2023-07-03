194119251e95517055602c89170706f0d098de35
Thibaut Durand <durand.tibo@gmail.com>
2023-07-02 23:04:05 -0700

    Add `is_iter_datapipe_config`

--- src/gravitorch/datapipes/__init__.py
+++ src/gravitorch/datapipes/__init__.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
-__all__ = ["clone_datapipe", "setup_datapipe"]
+__all__ = [
+    "clone_datapipe",
+    "setup_datapipe",
+    "is_iter_datapipe_config",
+    "setup_iter_datapipe",
+]
 
 from gravitorch.datapipes.factory import setup_datapipe
+from gravitorch.datapipes.iter import is_iter_datapipe_config, setup_iter_datapipe
 from gravitorch.datapipes.utils import clone_datapipe
