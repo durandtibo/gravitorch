2f8ac94fd60906ab5d4d33be3317ef75bcab9685
Thibaut Durand <durand.tibo@gmail.com>
2023-07-16 22:32:39 -0700

    Add `create_hypercube_vertex`

--- tests/unit/data/datacreators/test_hypercube.py
+++ tests/unit/data/datacreators/test_hypercube.py
@@ -9,6 +9,8 @@ from torch import Tensor
 
 from gravitorch import constants as ct
 from gravitorch.data.datacreators import HypercubeVertexDataCreator
+from gravitorch.data.datacreators.hypercube import create_hypercube_vertex
+from gravitorch.utils.seed import get_torch_generator
 
 SIZES = (1, 2, 4)
 
@@ -44,7 +46,7 @@ def test_hypercube_vertex_data_creator_num_classes(num_classes: int) -> None:
 @mark.parametrize("num_classes", (0, -1))
 def test_hypercube_vertex_data_creator_incorrect_num_classes(num_classes: int) -> None:
     with raises(ValueError, match="he number of classes .* has to be greater than 0"):
-        HypercubeVertexDataCreator(num_classes=0)
+        HypercubeVertexDataCreator(num_classes=num_classes)
 
 
 @mark.parametrize("feature_size", SIZES)
@@ -221,3 +223,93 @@ def test_hypercube_vertex_data_creator_state_repeat() -> None:
 def test_hypercube_vertex_data_creator_pickle_dumps() -> None:
     # Verify that the object can be serialized
     pickle.dumps(HypercubeVertexDataCreator())
+
+
+#############################################
+#     Tests for create_hypercube_vertex     #
+#############################################
+
+
+@mark.parametrize("num_examples", (0, -1))
+def test_create_hypercube_vertex_incorrect_num_examples(num_examples: int) -> None:
+    with raises(RuntimeError, match="The number of examples .* has to be greater than 0"):
+        create_hypercube_vertex(num_examples=num_examples)
+
+
+@mark.parametrize("num_classes", (0, -1))
+def test_create_hypercube_vertex_incorrect_num_classes(num_classes: int) -> None:
+    with raises(RuntimeError, match="he number of classes .* has to be greater than 0"):
+        create_hypercube_vertex(num_classes=num_classes)
+
+
+def test_create_hypercube_vertex_incorrect_feature_size() -> None:
+    with raises(
+        RuntimeError,
+        match="The feature dimension .* has to be greater or equal to the number of classes .*",
+    ):
+        create_hypercube_vertex(num_classes=50, feature_size=32)
+
+
+def test_create_hypercube_vertex_incorrect_noise_std() -> None:
+    with raises(
+        RuntimeError,
+        match="The standard deviation of the Gaussian noise .* has to be greater or equal than 0",
+    ):
+        create_hypercube_vertex(noise_std=-1)
+
+
+def test_create_hypercube_vertex_create() -> None:
+    data = create_hypercube_vertex(num_examples=10, num_classes=5, feature_size=8)
+    assert len(data) == 2
+    assert data[ct.TARGET].shape == (10,)
+    assert data[ct.TARGET].dtype == torch.long
+    assert data[ct.INPUT].shape == (10, 8)
+    assert data[ct.INPUT].dtype == torch.float
+
+
+@mark.parametrize("num_examples", SIZES)
+def test_create_hypercube_vertex_create_num_examples(num_examples: int) -> None:
+    data = create_hypercube_vertex(num_examples)
+    assert data[ct.TARGET].shape[0] == num_examples
+    assert data[ct.INPUT].shape[0] == num_examples
+
+
+@mark.parametrize("num_classes", SIZES)
+def test_create_hypercube_vertex_create_num_classes(num_classes: int) -> None:
+    targets = create_hypercube_vertex(num_examples=10, num_classes=num_classes)[ct.TARGET]
+    assert torch.min(targets) >= 0
+    assert torch.max(targets) < num_classes
+
+
+@mark.parametrize("feature_size", SIZES)
+def test_create_hypercube_vertex_create_feature_size(feature_size: int) -> None:
+    data = create_hypercube_vertex(num_examples=10, num_classes=1, feature_size=feature_size)
+    assert data[ct.INPUT].shape[1] == feature_size
+
+
+def test_create_hypercube_vertex_noise_std_0() -> None:
+    features = create_hypercube_vertex(num_examples=10, noise_std=0)[ct.INPUT]
+    assert torch.min(features) == 0
+    assert torch.max(features) == 1
+
+
+def test_create_hypercube_vertex_create_same_random_seed() -> None:
+    assert objects_are_equal(
+        create_hypercube_vertex(
+            num_examples=10, num_classes=5, feature_size=8, generator=get_torch_generator(1)
+        ),
+        create_hypercube_vertex(
+            num_examples=10, num_classes=5, feature_size=8, generator=get_torch_generator(1)
+        ),
+    )
+
+
+def test_create_hypercube_vertex_create_different_random_seeds() -> None:
+    assert not objects_are_equal(
+        create_hypercube_vertex(
+            num_examples=10, num_classes=5, feature_size=8, generator=get_torch_generator(1)
+        ),
+        create_hypercube_vertex(
+            num_examples=10, num_classes=5, feature_size=8, generator=get_torch_generator(2)
+        ),
+    )
