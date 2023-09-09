27cf9714ab749650433293ea129ca2455f209490
Thibaut Durand <durand.tibo@gmail.com>
2023-09-09 15:49:21 -0700

    Add `is_dataloader_creator_config` (#420)

--- src/gravitorch/creators/dataloader/factory.py
+++ src/gravitorch/creators/dataloader/factory.py
@@ -3,15 +3,48 @@ creators."""
 
 from __future__ import annotations
 
-__all__ = ["setup_dataloader_creator"]
+__all__ = ["is_dataloader_creator_config", "setup_dataloader_creator"]
 
 import logging
+
+from objectory.utils import is_object_config
 
 from gravitorch.creators.dataloader.base import BaseDataLoaderCreator
 from gravitorch.creators.dataloader.pytorch import AutoDataLoaderCreator
 from gravitorch.utils.format import str_target_object
 
 logger = logging.getLogger(__name__)
+
+
+def is_dataloader_creator_config(config: dict) -> bool:
+    r"""Indicates if the input configuration is a configuration for a
+    ``BaseDataLoaderCreator``.
+
+    This function only checks if the value of the key  ``_target_``
+    is valid. It does not check the other values. If ``_target_``
+    indicates a function, the returned type hint is used to check
+    the class.
+
+    Args:
+    ----
+        config (dict): Specifies the configuration to check.
+
+    Returns:
+    -------
+        bool: ``True`` if the input configuration is a configuration
+            for a ``BaseDataLoaderCreator`` object.
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from gravitorch.creators.dataloader import is_dataloader_creator_config
+        >>> is_dataloader_creator_config(
+        ...     {"_target_": "gravitorch.creators.dataloader.DataLoaderCreator"}
+        ... )
+        True
+    """
+    return is_object_config(config, BaseDataLoaderCreator)
 
 
 def setup_dataloader_creator(
