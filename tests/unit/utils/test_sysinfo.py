b411ad458f5e4eaa2f9eec4cd7ea1fbec332c86a
Thibaut Durand <durand.tibo@gmail.com>
2023-09-23 18:22:36 -0700

    Update sysinfo functions to not fail if psutil is not installed (#493)

--- tests/unit/utils/test_sysinfo.py
+++ tests/unit/utils/test_sysinfo.py
@@ -1,7 +1,7 @@
 import logging
 from unittest.mock import patch
 
-from pytest import LogCaptureFixture, raises
+from pytest import LogCaptureFixture
 
 from gravitorch.testing import psutil_available
 from gravitorch.utils.sysinfo import (
@@ -21,10 +21,11 @@ def test_cpu_human_summary() -> None:
     assert cpu_human_summary().startswith("CPU")
 
 
-def test_cpu_human_summary_no_psutil() -> None:
-    with patch("gravitorch.utils.imports.is_psutil_available", lambda *args: False):
-        with raises(RuntimeError, match="`psutil` package is required but not installed."):
-            cpu_human_summary()
+def test_cpu_human_summary_no_psutil(caplog: LogCaptureFixture) -> None:
+    with caplog.at_level(logging.WARNING):
+        with patch("gravitorch.utils.sysinfo.is_psutil_available", lambda *args: False):
+            assert cpu_human_summary() == "CPU - N/A"
+            assert len(caplog.messages) == 1
 
 
 #####################################
@@ -39,10 +40,11 @@ def test_log_system_info(caplog: LogCaptureFixture) -> None:
     assert len(caplog.messages) == 3
 
 
-def test_log_system_info_no_psutil() -> None:
-    with patch("gravitorch.utils.imports.is_psutil_available", lambda *args: False):
-        with raises(RuntimeError, match="`psutil` package is required but not installed."):
+def test_log_system_info_no_psutil(caplog: LogCaptureFixture) -> None:
+    with caplog.at_level(logging.WARNING):
+        with patch("gravitorch.utils.sysinfo.is_psutil_available", lambda *args: False):
             log_system_info()
+            assert len(caplog.messages) == 3
 
 
 ###############################################
@@ -55,10 +57,11 @@ def test_swap_memory_human_summary() -> None:
     assert swap_memory_human_summary().startswith("swap memory")
 
 
-def test_swap_memory_human_summary_no_psutil() -> None:
-    with patch("gravitorch.utils.imports.is_psutil_available", lambda *args: False):
-        with raises(RuntimeError, match="`psutil` package is required but not installed."):
-            swap_memory_human_summary()
+def test_swap_memory_human_summary_no_psutil(caplog: LogCaptureFixture) -> None:
+    with caplog.at_level(logging.WARNING):
+        with patch("gravitorch.utils.sysinfo.is_psutil_available", lambda *args: False):
+            assert swap_memory_human_summary() == "swap memory - N/A"
+            assert len(caplog.messages) == 1
 
 
 ##################################################
@@ -71,7 +74,8 @@ def test_virtual_memory_human_summary() -> None:
     assert virtual_memory_human_summary().startswith("virtual memory")
 
 
-def test_virtual_memory_human_summary_no_psutil() -> None:
-    with patch("gravitorch.utils.imports.is_psutil_available", lambda *args: False):
-        with raises(RuntimeError, match="`psutil` package is required but not installed."):
-            virtual_memory_human_summary()
+def test_virtual_memory_human_summary_no_psutil(caplog: LogCaptureFixture) -> None:
+    with caplog.at_level(logging.WARNING):
+        with patch("gravitorch.utils.sysinfo.is_psutil_available", lambda *args: False):
+            assert virtual_memory_human_summary() == "virtual memory - N/A"
+            assert len(caplog.messages) == 1
