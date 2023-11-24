19aeec6f05e3cf8d9a7a5afc7ec87409caea5cc9
Thibaut Durand <durand.tibo@gmail.com>
2023-11-23 17:20:20 -0800

    Remove experimental batch loader

--- src/gravitorch/experimental/batchloader/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["BaseBatchLoader", "IterableBatchLoader"]
-
-from gravitorch.experimental.batchloader.base import BaseBatchLoader
-from gravitorch.experimental.batchloader.iterable import IterableBatchLoader
