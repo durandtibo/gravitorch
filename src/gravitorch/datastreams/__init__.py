2a80262b9077d2f1f6e1abe952a427b94fae9781
Thibaut Durand <durand.tibo@gmail.com>
2023-11-25 11:23:35 -0800

    Remove `BaseDataStream`

--- src/gravitorch/datastreams/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["BaseDataStream", "DataLoaderDataStream", "IterableDataStream"]
-
-from gravitorch.datastreams.base import BaseDataStream
-from gravitorch.datastreams.dataloader import DataLoaderDataStream
-from gravitorch.datastreams.iterable import IterableDataStream
